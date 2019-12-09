52e1526586f7249bdf99cc5e72590c4988f655e2
Yeison Gonzalez <yeison.gonzalez01@hotmail.com>
2019-12-09 18:37:37 -0500

    feat(app) add router
    add router with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
 import { HelloWorld } from './firts-component/first.component';
@@ -9,18 +10,26 @@ import { NewContentComponent } from './new-content/new-content.component';
 import { UserComponent } from './user/user.component';
 
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const routes: Route[] = [
+  {path: '', component: AppComponent},
+  {path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorld,
     NewContentComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
