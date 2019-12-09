488fd50dc9ad0da87613a4619e3b7c56313826d6
Yeison Gonzalez <yeison.gonzalez01@hotmail.com>
2019-12-09 17:56:59 -0500

    feat(app) create crud
    add crud with angular and use events

--- src/app/new-content/new-content.component.ts
+++ src/app/new-content/new-content.component.ts
@@ -6,18 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-content.component.css']
 })
 export class NewContentComponent implements OnInit {
-  users = ['ryan', 'joe', 'cameron']
+  users: string[] = ['ryan', 'joe', 'cameron']
   activated: boolean = true;
-  name: string = 'Ryan Ray'
-  age: number;
+  name: string = 'John Carter';
+  age: number = 28;
   address:  {
     street: string;
     city: string;
   };
   hobbies: string[];
 
+  sayHello() {
+    alert('Hello user');
+  }
+
+  addUser(newUser) {
+    if (newUser.value) {
+      this.users.push(newUser.value);
+      newUser.value = '';
+      newUser.focus();
+    }
+    return false;
+  }
+
+  deleteUser(user) {
+    for(let i = 0; i < this.users.length; i++) {
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+  }
+
   constructor() {
-    this.age = 28;
     this.address = {
       street: 'Backer Street',
       city: 'London'
